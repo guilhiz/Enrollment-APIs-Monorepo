5fb385a287a02cb3aacd8e10141d65988ca6d760
guilhiz <guilhermevilelasobral@gmail.com>
2024-04-27 22:09:31 -0300

    test: add more validation for routers

--- api-age-group/app/tests/routers/test_age_groups_routes.py
+++ api-age-group/app/tests/routers/test_age_groups_routes.py
@@ -3,22 +3,25 @@ from app.main import app
 
 client = TestClient(app)
 
+def test_retrieve_list_of_age_groups_successfully():
+    age_groups_response = client.get("/age-groups")
+    assert age_groups_response.status_code == 200
+    age_groups_data = age_groups_response.json()
+    assert "max_age" in age_groups_data[0]
+    assert len(age_groups_data) > 0
 
-def test_list_age_groups():
-    response = client.get("/age-groups")
-    assert response.status_code == 200
-    data = response.json()
-    assert "id" in data[0]
-    assert len(data) > 0
+def test_add_new_age_group_successfully():
+    add_response = client.post("/age-groups", json={"min_age": 7, "max_age": 51})
+    assert add_response.status_code == 201
 
+def test_add_new_age_group_fails_due_to_missing_parameters():
+    missing_params_response = client.post("/age-groups", json={})
+    assert missing_params_response.status_code == 422
 
-def test_add_age_group():
-    response = client.post("/age-groups", json={"id": 12, "min_age": 7, "max_age": 51})
-    assert response.status_code == 201
-    data = response.json()
-    assert "id" in data
+def test_delete_existing_age_group_successfully():
+    delete_response = client.delete("/age-groups/662d676dfae3b87935a62140")
+    assert delete_response.status_code == 204
 
-
-def test_delete_age_group():
-    response = client.delete("/age-groups/12")
-    assert response.status_code == 204
+def test_delete_nonexistent_age_group():
+    nonexistent_delete_response = client.delete("/age-groups/662d676dfae3b87935a62111")
+    assert nonexistent_delete_response.status_code == 404
--- api-enrollment/app/tests/routers/test_enrollment_routes.py
+++ api-enrollment/app/tests/routers/test_enrollment_routes.py
@@ -4,13 +4,22 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_get_enrollment_status():
-    response = client.get("/enrollments/12345678901")
-    assert response.status_code == 404
+def test_get_enrollment_status_successful():
+    response_success = client.get("/enrollments/12345678901")
+    assert response_success.status_code == 200
 
+def test_get_enrollment_status_not_found():
+    response_not_found = client.get("/enrollments/12345678922")
+    assert response_not_found.status_code == 404
 
-def test_request_enrollment():
-    response = client.post(
+def test_request_enrollment_successful():
+    response_success = client.post(
+        "/enrollments", json={"name": "John Doe", "cpf": "12345678942", "age": 30, "enrollment_status": "active"}
+    )
+    assert response_success.status_code == 201
+
+def test_request_enrollment_cpf_already_exists():
+    response_cpf_exists = client.post(
         "/enrollments", json={"name": "John Doe", "cpf": "12345678901", "age": 30, "enrollment_status": "active"}
     )
-    assert response.status_code == 201
+    assert response_cpf_exists.status_code == 400
