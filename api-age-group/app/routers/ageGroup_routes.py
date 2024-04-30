3472bccb726bbfcbac6770d0b2ad7ca76679ddda
guilhiz <guilhermevilelasobral@gmail.com>
2024-04-30 01:28:08 -0300

    feat: add Basic Auth to age-groups API

--- api-age-group/app/routers/ageGroup_routes.py
+++ api-age-group/app/routers/ageGroup_routes.py
@@ -1,25 +1,26 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, HTTPException, Response, status, Depends
 
 from app.config.mongodb import db_manager
+from app.config.auth.auth import verify_credentials
 from app.models.age_group import AgeGroup, AgeGroupOut
 
 router = APIRouter(prefix="/age-groups", tags=["Age Groups"])
 
 
 @router.get("/", response_model=List[AgeGroupOut], description="Get all age groups")
-def get_age_groups():
+def get_age_groups(_: str = Depends(verify_credentials)):
     return db_manager.list_items()
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, description="Create a new age group")
-def add_age_group(age_group: AgeGroup):
+def add_age_group(age_group: AgeGroup, _: str = Depends(verify_credentials)):
     return db_manager.add_item(dict(age_group))
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, description="Delete an age group")
-def delete_age_group(id: str):
+def delete_age_group(id: str, _: str = Depends(verify_credentials)):
     result = db_manager.delete_item(id)
     if result is None:
         raise HTTPException(status_code=404, detail="Age group not found")
--- api-age-group/app/tests/routers/test_age_groups_routes.py
+++ api-age-group/app/tests/routers/test_age_groups_routes.py
@@ -1,3 +1,4 @@
+from app.config.auth.auth import get_basic_auth_header
 from fastapi.testclient import TestClient
 from app.config.mongodb import db_manager
 
@@ -5,9 +6,10 @@ from app.main import app
 
 client = TestClient(app)
 
+auth_header = get_basic_auth_header("admin", "secret")
 
 def test_retrieve_list_of_age_groups_successfully():
-    age_groups_response = client.get("/age-groups")
+    age_groups_response = client.get("/age-groups", headers=auth_header)
     assert age_groups_response.status_code == 200
     age_groups_data = age_groups_response.json()
     assert "max_age" in age_groups_data[0]
@@ -15,24 +17,24 @@ def test_retrieve_list_of_age_groups_successfully():
 
 
 def test_add_new_age_group_successfully():
-    add_response = client.post("/age-groups", json={"min_age": 7, "max_age": 51})
+    add_response = client.post("/age-groups", json={"min_age": 7, "max_age": 51}, headers=auth_header)
     assert add_response.status_code == 201
     age_group_data = add_response.json()
     assert "max_age" in age_group_data
     db_manager.delete_item(age_group_data["id"])
 
 def test_add_new_age_group_fails_due_to_missing_parameters():
-    missing_params_response = client.post("/age-groups", json={})
+    missing_params_response = client.post("/age-groups", json={}, headers=auth_header)
     assert missing_params_response.status_code == 422
 
 
 def test_delete_existing_age_group_successfully():
     age_group_id = db_manager.add_item({"min_age": 7, "max_age": 51})["id"]
 
-    delete_response = client.delete(f"/age-groups/{age_group_id}")
+    delete_response = client.delete(f"/age-groups/{age_group_id}", headers=auth_header)
     assert delete_response.status_code == 204
 
 
 def test_delete_nonexistent_age_group():
-    nonexistent_delete_response = client.delete("/age-groups/662d676dfae3b87935a62111")
+    nonexistent_delete_response = client.delete("/age-groups/662d676dfae3b87935a62111", headers=auth_header)
     assert nonexistent_delete_response.status_code == 404
