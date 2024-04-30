18378d203508630aa08a5518b0441653d25e2205
guilhiz <guilhermevilelasobral@gmail.com>
2024-04-30 06:44:35 -0300

    feat: more add validation tests for Enrollment model

--- api-enrollment/app/models/enrollment.py
+++ api-enrollment/app/models/enrollment.py
@@ -1,13 +1,18 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class Enrollment(BaseModel):
-    name: str = Field(min_length=3, max_length=100)
-    cpf: str = Field(min_length=11, max_length=14)
-    age: int = Field(gt=0)
+    name: str = Field(..., min_length=3, max_length=100)
+    cpf: str = Field(...,min_length=11, max_length=11)
+    age: int = Field(..., gt=0)
 
+    @validator('cpf')
+    def cpf_must_be_numeric(cls, v, ):
+        if not v.isdigit():
+            raise ValueError('CPF must only contain digits')
+        return v
 
 class EnrollmentOut(BaseModel):
     id: str
@@ -16,4 +21,3 @@ class EnrollmentOut(BaseModel):
     age: int
     enrollment_status: str
     error_message: Optional[str] = None
-
--- api-enrollment/app/tests/models/test_enrollment.py
+++ api-enrollment/app/tests/models/test_enrollment.py
@@ -9,19 +9,35 @@ def test_enrollment_model():
     assert enrollment.dict() == {"name": "John Doe", "cpf": "12345678901", "age": 30}
 
 
-def test_enrollment_invalid_name():
+def test_enrollment_with_too_short_name():
     with pytest.raises(ValidationError):
         Enrollment(name="Jo", cpf="12345678901", age=30)
 
 
-def test_enrollment_invalid_cpf():
+def test_enrollment_with_too_long_name():
+    with pytest.raises(ValidationError):
+        Enrollment(name="J" * 101, cpf="12345678901", age=30)
+
+
+def test_enrollment_with_invalid_cpf_length():
     with pytest.raises(ValidationError):
         Enrollment(name="John Doe", cpf="123456789", age=30)
 
+
     with pytest.raises(ValidationError):
-        Enrollment(name="John Doe", cpf="123.456.789-012", age=30)
+        Enrollment(name="John Doe", cpf="123456789012", age=30)
 
 
-def test_enrollment_invalid_age():
+def test_enrollment_with_non_numeric_cpf():
+    with pytest.raises(ValidationError):
+        Enrollment(name="John Doe", cpf="123R456B789", age=30)
+
+
+def test_enrollment_with_negative_age():
+    with pytest.raises(ValidationError):
+        Enrollment(name="John Doe", cpf="12345678901", age=-1)
+
+
+def test_enrollment_with_zero_age():
     with pytest.raises(ValidationError):
         Enrollment(name="John Doe", cpf="12345678901", age=0)
